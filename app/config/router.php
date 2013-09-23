cfe529c42c3c4e14b212225d1448deb06bd0c62d
OanhNN <OanhNN@OanhNN-PC>
2013-09-23 14:25:24 +0700

    Update simple demo controller

--- app/config/router.php
+++ app/config/router.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * 
+ * Config router of app
+ * <code>
+ * '/some/path' => 'className:methodName',
+ * '/some/path' => array('className:methodName', 'get'),
+ * '/some/path' => array('className:methodName', 'get', function(){}),
+ * </code>
  */
 return array(
-    '/' => array('Api\\Controller\\Test:index')
+    '/' => array('Demo\\Frontend\\Controller\\Index:index', 'GET')
 );
---
+++ src/Demo/Frontend/Controller/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Demo\Frontend\Controller;
+/**
+ * Description of Index
+ *
+ * @author OanhNN <oanhnn@rikkeisoft.com>
+ */
+class Index extends \SlimController\SlimController
+{
+    /**
+     * Index action
+     * URL: /
+     * 
+     * @return void
+     */
+    public function indexAcrion(){
+        $name = $this->param('name', 'get');
+        if ($name)
+            echo "Hello {$name}!";
+        else 
+            echo "Hello Slim Framework";
+    }
+}
+
