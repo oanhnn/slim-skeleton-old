8a942d3a427e691309f118563d3998782a1256a9
OanhNN <OanhNN@OanhNN-PC>
2013-12-04 17:59:01 +0700

    Example using Twig template

--- public/index.php
+++ public/index.php
@@ -38,7 +38,7 @@ $app->get('/', function () use ($app) {
     // Sample log message
     $app->log->info("Slim-Skeleton '/' route");
     // Render index view
-    $app->render('index.html');
+    $app->render('index.html.twig', array('name' => 'slim framework'));
 });
 
 // Run app
