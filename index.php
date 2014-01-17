1c9f9e88f3646c2f5d379bd65af0a2873fed30fb
OanhNN <OanhNN@OanhNN-PC>
2014-01-17 15:08:34 +0700

    Fix htaccess follow http://docs.slimframework.com/#Route-URL-Rewriting

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// define a working directory
-defined('APP_PATH') || define('APP_PATH', dirname(__FILE__) . '/app'); // PHP v5.3+
-// define application environment
-defined('APP_PATH') || define('APP_ENV', getenv('APP_ENV') ? getenv('APP_ENV') : 'production');
-
-require_once APP_PATH . '/../public/index.php';
