8ffc99933fedb67b5e0ecfa2aca4a5891b47eaa2
oanhnn <oanhnn@rikkeisoft.com>
2015-12-31 14:28:09 +0700

    Add demo assets

--- gulpfile.js
+++ gulpfile.js
@@ -12,24 +12,24 @@ var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 
 var pkg = require('./composer.json');
-var banner = (_.template('/*! <%=name%> v<%=version%> | License <%=license%> | <%=homepage%> */\n\n'))(pkg);
+var banner = (_.template('/*! <%=name%> v<%=version%> | License <%=license%> | <%=homepage%> */\n'))(pkg);
 var dirs = {
     src: 'app/assets',
-    dist: 'public/assets'
+    dist: 'public'
 };
 // -----------------------------------------------------------------------------
 // Gulp tasks
 // -----------------------------------------------------------------------------
 gulp.task('clean', function () {
     require('del')([
-        dirs.dist + '/**',
-        '!' + dirs.dist,
-        '!' + dirs.dist + '/.gitignore'
+        dirs.dist + '/css',
+        dirs.dist + '/js'
     ]);
 });
 
 gulp.task('create:dir', function () {
-    fs.mkdirSync(path.resolve('tmp/build'), '0755');
+    fs.mkdirSync(path.resolve(dirs.dist + '/css'), '0755');
+    fs.mkdirSync(path.resolve(dirs.dist + '/js'), '0755');
 });
 
 gulp.task('jshint', function () {
@@ -39,6 +39,15 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('build:css', function () {
+    return gulp.src(dirs.src + '/css/**/*.css')
+        .pipe(plugins.minifyCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(plugins.header(banner))
+        .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+gulp.task('build:sass', function () {
     return gulp.src(dirs.src + '/sass/**/*.scss')
         .pipe(plugins.sass())
         .pipe(plugins.minifyCss({
@@ -57,13 +66,17 @@ gulp.task('build:js', function () {
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function () {
     gulp.watch(dirs.src + '/js/**/*.js', ['jshint', 'build:js']);
-    gulp.watch(dirs.src + '/sass/**/*.scss', ['build:css']);
+    gulp.watch(dirs.src + '/sass/**/*.scss', ['build:sass']);
+    gulp.watch(dirs.src + '/css/**/*.css', ['build:css']);
 });
 
 gulp.task('build', function (done) {
     runSequence(
-        ['clean', 'jshint'], ['build:css', 'build:js'],
-        done);
+        ['clean', 'jshint'],
+        ['create:dir'],
+        ['build:sass', 'build:css', 'build:js'],
+        done
+    );
 });
 
 gulp.task('default', ['build']);
